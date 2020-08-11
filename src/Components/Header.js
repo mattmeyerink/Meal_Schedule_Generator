356cea25f5106971a760dd873ee68aef4600f973
Matthew Meyerink <meyerink@umich.edu>
2020-08-11 10:04:08 -0500

    Added basic header

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Components.css'
+
+function Header() {
+    return(
+        <header>
+            <h1>
+                Customizable Dinner Scheduler
+            </h1>
+            <p>
+                Taking the "I don't knows out of meal planning"
+            </p>
+        </header>
+    )
+}
+
+export default Header
