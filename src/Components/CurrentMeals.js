8ed4d36c8e7c41e863819c0a202952b2f083e82c
Matthew Meyerink <meyerink@umich.edu>
2020-08-12 14:22:53 -0500

    Added meal source and changed CurrentMeals from a function to a class

--- src/Components/CurrentMeals.js
+++ src/Components/CurrentMeals.js
@@ -1,71 +1,72 @@
 import React from 'react'
 
-function CurrentMeals(props) {
-    let mondayMeal
-    let tuesdayMeal
-    let wednesdayMeal
-    let thursdayMeal
-    let fridayMeal
-    let saturdayMeal
-    let sundayMeal
-
-    if (props.mealsAreAssigned) {
-        mondayMeal = props.assignedMeals[0].recipe
-        tuesdayMeal = props.assignedMeals[1].recipe
-        wednesdayMeal = props.assignedMeals[2].recipe
-        thursdayMeal = props.assignedMeals[3].recipe
-        fridayMeal = props.assignedMeals[4].recipe
-        saturdayMeal = props.assignedMeals[5].recipe
-        sundayMeal = props.assignedMeals[6].recipe
+class CurrentMeals extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            mondayMeal: this.props.data.mondayMeal,
+            tuesdayMeal: this.props.data.tuesdayMeal,
+            wednesdayMeal: this.props.data.wednesdayMeal,
+            thursdayMeal: this.props.data.thursdayMeal,
+            fridayMeal: this.props.data.fridayMeal,
+            saturdayMeal: this.props.data.saturdayMeal,
+            sundayMeal: this.props.data.sundayMeal
+        }
+        this.printingIngredients = this.printingIngredients.bind(this)
     }
 
+    printingIngredients(ingredientList) {
+        return ingredientList.map(item => {
+            return <li key={item}>{item}</li>
+        })
+    }
 
+    submitNewMeal(meal) {
+        meal = {}
+    }
 
-    function printingMealSchedule() {
-        function printingIngredients(ingredientList) {
-            return ingredientList.map(item => {
-                return <li key={item}>{item}</li>
-            })
-        }
+    render() {
         return(
             <div>
+                <h1>This Week's Meals</h1>
                 <h3>Monday</h3>
-                <p>{mondayMeal.label}</p>
-                {printingIngredients(mondayMeal.ingredientLines)}
+                <p>{this.state.mondayMeal.label}</p>
+                {this.printingIngredients(this.state.mondayMeal.ingredientLines)}
+                <p>Original Meal Source: {this.state.mondayMeal.source}</p>
 
                 <h3>Tuesday</h3>
-                <p>{tuesdayMeal.label}</p>
-                {printingIngredients(tuesdayMeal.ingredientLines)}
+                <p>{this.state.tuesdayMeal.label}</p>
+                {this.printingIngredients(this.state.tuesdayMeal.ingredientLines)}
+                <p>Original Meal Source: {this.state.tuesdayMeal.source}</p>
 
                 <h3>Wednesday</h3>
-                <p>{wednesdayMeal.label}</p>
-                {printingIngredients(wednesdayMeal.ingredientLines)}
+                <p>{this.state.wednesdayMeal.label}</p>
+                {this.printingIngredients(this.state.wednesdayMeal.ingredientLines)}
+                <p>Original Meal Source: {this.state.wednesdayMeal.source}</p>
 
                 <h3>Thursday</h3>
-                <p>{thursdayMeal.label}</p>
-                {printingIngredients(thursdayMeal.ingredientLines)}
+                <p>{this.state.thursdayMeal.label}</p>
+                {this.printingIngredients(this.state.thursdayMeal.ingredientLines)}
+                <p>Original Meal Source: {this.state.thursdayMeal.source}</p>
 
                 <h3>Friday</h3>
-                <p>{fridayMeal.label}</p>
-                {printingIngredients(fridayMeal.ingredientLines)}
+                <p>{this.state.fridayMeal.label}</p>
+                {this.printingIngredients(this.state.fridayMeal.ingredientLines)}
+                <p>Original Meal Source: {this.state.fridayMeal.source}</p>
 
                 <h3>Saturday</h3>
-                <p>{saturdayMeal.label}</p>
-                {printingIngredients(saturdayMeal.ingredientLines)}
+                <p>{this.state.saturdayMeal.label}</p>
+                {this.printingIngredients(this.state.saturdayMeal.ingredientLines)}
+                <p>Original Meal Source: {this.state.saturdayMeal.source}</p>
 
                 <h3>Sunday</h3>
-                <p>{sundayMeal.label}</p>
-                {printingIngredients(sundayMeal.ingredientLines)}
+                <p>{this.state.sundayMeal.label}</p>
+                {this.printingIngredients(this.state.sundayMeal.ingredientLines)}
+                <p>Original Meal Source: {this.state.sundayMeal.source}</p>
+
             </div>
         )
     }
-    return(
-        <div>
-            {props.mealsAreAssigned ?
-            printingMealSchedule() :
-            "Meals are not assigned"}
-        </div>
-    )
 }
 
 export default CurrentMeals
--- src/Components/MealScheduler.js
+++ src/Components/MealScheduler.js
@@ -8,13 +8,21 @@ class MealScheduler extends React.Component {
         this.state = {
             possibleMeals: [],
             assignedMeals: [],
-            mealsAreAssigned: false
+            mealsAreAssigned: false,
+
+            mondayMeal: {},
+            tuesdayMeal: {},
+            wednesdayMeal: {},
+            thursdayMeal: {},
+            fridayMeal: {},
+            saturdayMeal: {},
+            sundayMeal: {}
         }
         this.assignMeals = this.assignMeals.bind(this)
     }
 
     componentDidMount() {
-        const url = "https://api.edamam.com/search?q=balanced&app_id=6509b4bd&" +
+        const url = "https://api.edamam.com/search?q=Dinner&app_id=6509b4bd&" +
         "app_key=f2d99a5d7c8852983396755420e3d9d9&calories=500-1000"
         fetch(url)
             .then(response => response.json())
@@ -40,16 +48,23 @@ class MealScheduler extends React.Component {
             mealsAlreadyUsed.push(randomMeal)
         }
         this.setState({mealsAreAssigned: true})
+        this.setState({mondayMeal: this.state.assignedMeals[0].recipe})
+        this.setState({tuesdayMeal: this.state.assignedMeals[1].recipe})
+        this.setState({wednesdayMeal: this.state.assignedMeals[2].recipe})
+        this.setState({thursdayMeal: this.state.assignedMeals[3].recipe})
+        this.setState({fridayMeal: this.state.assignedMeals[4].recipe})
+        this.setState({saturdayMeal: this.state.assignedMeals[5].recipe})
+        this.setState({sundayMeal: this.state.assignedMeals[6].recipe})
     }
 
     render() {
         return(
             <div>
+                {this.state.mealsAreAssigned ?
+                <CurrentMeals data = {this.state} /> :
                 <button onClick={this.assignMeals}>
                     Schedule Your Meals!
-                </button>
-                <CurrentMeals assignedMeals={this.state.assignedMeals}
-                mealsAreAssigned={this.state.mealsAreAssigned}/>
+                </button>}
             </div>
         )
     }
