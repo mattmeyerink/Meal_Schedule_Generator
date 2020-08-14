ea03b7f688aab02e6fbf78345c19d086fb27c7b7
Matthew Meyerink <meyerink@umich.edu>
2020-08-14 17:59:24 -0500

    Cleaned up logic with change meal functionality

--- src/App.js
+++ src/App.js
@@ -52,7 +52,6 @@ class App extends React.Component {
         }
     }
 
-
     render() {
         return (
             <div>
--- src/Components/CurrentMeals.js
+++ src/Components/CurrentMeals.js
@@ -8,12 +8,19 @@ class CurrentMeals extends React.Component {
         this.state = {
             mealsAreAssigned: this.props.data.mealsAreAssigned,
             mondayMeal: this.props.data.mondayMeal,
+            mondayMealEditing: false,
             tuesdayMeal: this.props.data.tuesdayMeal,
+            tuesdayMealEditing: false,
             wednesdayMeal: this.props.data.wednesdayMeal,
+            wednesdayMealEditing: false,
             thursdayMeal: this.props.data.thursdayMeal,
+            thursdayMealEditing: false,
             fridayMeal: this.props.data.fridayMeal,
+            fridayMealEditing: false,
             saturdayMeal: this.props.data.saturdayMeal,
+            saturdayMealEditing: false,
             sundayMeal: this.props.data.sundayMeal,
+            sundayMealEditing: false,
             mealBeingChanged: ""
         }
         this.printingIngredients = this.printingIngredients.bind(this)
@@ -26,90 +33,132 @@ class CurrentMeals extends React.Component {
         })
     }
 
-    changingMeal() {
-        this.setState({mealBeingChanged: !this.state.mealBeingChanged})
+    changingMeal(meal) {
+        if (meal === 'monday') {
+            this.setState({mondayMealEditing: !this.state.mondayMealEditing})
+        }
+        else if (meal === 'tuesday') {
+            this.setState({tuesdayMealEditing: !this.state.tuesdayMealEditing})
+        }
+        else if (meal === 'wednesday') {
+            this.setState({wednesdayMealEditing: !this.state.wednesdayMealEditing})
+        }
+        else if (meal === 'thursday') {
+            this.setState({thursdayMealEditing: ! this.state.thursdayMealEditing})
+        }
+        else if (meal === 'friday') {
+            this.setState({fridayMealEditing: !this.state.fridayMealEditing})
+        }
+        else if (meal === 'saturday') {
+            this.setState({saturdayMealEditing: !this.state.saturdayMealEditing})
+        }
+        else if (meal === 'sunday') {
+            this.setState({sundayMealEditing: !this.state.sundayMealEditing})
+        }
     }
 
     render() {
         return(
             <div>
-                {!this.state.mealBeingChanged ?
+                <h1>This Week's Meals</h1>
+
+                <h2>Monday</h2>
+                {!this.state.mondayMealEditing ?
                 <div>
-                    <h1>This Week's Meals</h1>
-                    <h3>Monday: {this.state.mondayMeal.label}</h3>
+                    <h3>{this.state.mondayMeal.label}</h3>
                     <p>Ingredients</p>
                     {this.printingIngredients(this.state.mondayMeal.ingredientLines)}
                     <p>Original Meal Source: {this.state.mondayMeal.source}</p>
-                    <button onClick={this.changingMeal}>Change Meal</button>
+                    <button onClick={() => this.changingMeal('monday')}>
+                    Change Meal
+                    </button>
                 </div> :
                 <AddMeal meal={this.state.mondayMeal}
-                changingMeal={this.changingMeal}/>}
+                changingMeal={() => this.changingMeal('monday')}/>}
 
-                {!this.state.mealBeingChanged ?
+                <h2>Tuesday</h2>
+                {!this.state.tuesdayMealEditing ?
                 <div>
-                    <h3>Tuesday: {this.state.tuesdayMeal.label}</h3>
+                    <h3>{this.state.tuesdayMeal.label}</h3>
                     <p>Ingredients</p>
                     {this.printingIngredients(this.state.tuesdayMeal.ingredientLines)}
                     <p>Original Meal Source: {this.state.tuesdayMeal.source}</p>
-                    <button onClick={this.changingMeal}>Change Meal</button>
+                    <button onClick={() => this.changingMeal('tuesday')}>
+                    Change Meal
+                    </button>
                 </div> :
                 <AddMeal meal={this.state.tuesdayMeal}
-                changingMeal={this.changingMeal}/>}
+                changingMeal={() => this.changingMeal('tuesday')}/>}
 
-                {!this.state.mealBeingChanged ?
+                <h2>Wednesday</h2>
+                {!this.state.wednesdayMealEditing ?
                 <div>
-                    <h3>Wednesday: {this.state.wednesdayMeal.label}</h3>
+                    <h3>{this.state.wednesdayMeal.label}</h3>
                     <p>Ingredients</p>
                     {this.printingIngredients(this.state.wednesdayMeal.ingredientLines)}
                     <p>Original Meal Source: {this.state.wednesdayMeal.source}</p>
-                    <button onClick={this.changingMeal}>Change Meal</button>
+                    <button onClick={() => this.changingMeal('wednesday')}>
+                    Change Meal
+                    </button>
                 </div>:
                 <AddMeal meal={this.state.wednesdayMeal}
-                changingMeal={this.changingMeal}/>}
+                changingMeal={() => this.changingMeal('wednesday')}/>}
 
-                {!this.state.mealBeingChanged ?
+                <h2>Thursday</h2>
+                {!this.state.thursdayMealEditing ?
                 <div>
-                    <h3>Thursday: {this.state.thursdayMeal.label}</h3>
+                    <h3>{this.state.thursdayMeal.label}</h3>
                     <p>Ingredients</p>
                     {this.printingIngredients(this.state.thursdayMeal.ingredientLines)}
                     <p>Original Meal Source: {this.state.thursdayMeal.source}</p>
-                    <button onClick={this.changingMeal}>Change Meal</button>
+                    <button onClick={() => this.changingMeal('thursday')}>
+                    Change Meal
+                    </button>
                 </div>:
                 <AddMeal meal={this.state.thursdayMeal}
-                changingMeal={this.changingMeal}/>}
+                changingMeal={() => this.changingMeal('thursday')}/>}
 
-                {!this.state.mealBeingChanged ?
+                <h2>Friday</h2>
+                {!this.state.fridayMealEditing ?
                 <div>
-                    <h3>Friday: {this.state.fridayMeal.label}</h3>
+                    <h3>{this.state.fridayMeal.label}</h3>
                     <p>Instructions</p>
                     {this.printingIngredients(this.state.fridayMeal.ingredientLines)}
                     <p>Original Meal Source: {this.state.fridayMeal.source}</p>
-                    <button onClick={this.changingMeal}>Change Meal</button>
+                    <button onClick={() => this.changingMeal('friday')}>
+                    Change Meal
+                    </button>
                 </div> :
                 <AddMeal meal={this.state.fridayMeal}
-                changingMeal={this.changingMeal}/>}
+                changingMeal={() => this.changingMeal('friday')}/>}
 
-                {!this.state.mealBeingChanged ?
+                <h2>Saturday</h2>
+                {!this.state.saturdayMealEditing ?
                 <div>
-                    <h3>Saturday: {this.state.saturdayMeal.label}</h3>
+                    <h3>{this.state.saturdayMeal.label}</h3>
                     <p>Instructions</p>
                     {this.printingIngredients(this.state.saturdayMeal.ingredientLines)}
                     <p>Original Meal Source: {this.state.saturdayMeal.source}</p>
-                    <button onClick={this.changingMeal}>Change Meal</button>
+                    <button onClick={() => this.changingMeal('saturday')}>
+                    Change Meal
+                    </button>
                 </div>:
                 <AddMeal meal={this.state.saturdayMeal}
-                changingMeal={this.changingMeal}/>}
+                changingMeal={() => this.changingMeal('saturday')}/>}
 
-                {!this.state.mealBeingChanged ?
+                <h2>Sunday</h2>
+                {!this.state.sundayMealEditing ?
                 <div>
-                    <h3>Sunday: {this.state.sundayMeal.label}</h3>
+                    <h3>{this.state.sundayMeal.label}</h3>
                     <p>Instructions</p>
                     {this.printingIngredients(this.state.sundayMeal.ingredientLines)}
                     <p>Original Meal Source: {this.state.sundayMeal.source}</p>
-                    <button onClick={this.changingMeal}>Change Meal</button>
+                    <button onClick={() => this.changingMeal('sunday')}>
+                    Change Meal
+                    </button>
                 </div> :
                 <AddMeal meal={this.state.sundayMeal}
-                changingMeal={this.changingMeal}/>}
+                changingMeal={() => this.changingMeal('sunday')}/>}
 
             </div>
         )
