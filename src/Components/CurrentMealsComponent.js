86c5b00ec76a0c9c29f8b8c1ec8c44a6f584ca4b
Matthew Meyerink <meyerink@umich.edu>
2020-08-16 13:29:32 -0500

    Finished implementing Change Meal logic

--- src/Components/CurrentMealsComponent.js
+++ src/Components/CurrentMealsComponent.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import AddMeal from './AddMeal.js'
-
 function CurrentMealsComponent(props) {
     return(
         <div>
@@ -10,10 +8,10 @@ function CurrentMealsComponent(props) {
             <h2>Monday</h2>
             {!props.data.mondayMealEditing ?
             <div>
-                <h3>{props.data.mondayMeal.label}</h3>
+                <h3>{props.data.mondayMealLabel}</h3>
                 <p>Ingredients</p>
-                {props.printingIngredients(props.data.mondayMeal.ingredientLines)}
-                <p>Original Meal Source: {props.data.mondayMeal.source}</p>
+                {props.printingIngredients(props.data.mondayMealIngredients)}
+                <p>Original Meal Source: {props.data.mondayMealSource}</p>
                 <button onClick={() => props.changingMeal('monday')}>
                 Change Meal
                 </button>
@@ -45,86 +43,206 @@ function CurrentMealsComponent(props) {
 
             {!props.data.tuesdayMealEditing ?
             <div>
-                <h3>{props.data.tuesdayMeal.label}</h3>
+                <h3>{props.data.tuesdayMealLabel}</h3>
                 <p>Ingredients</p>
-                {props.printingIngredients(props.data.tuesdayMeal.ingredientLines)}
-                <p>Original Meal Source: {props.data.tuesdayMeal.source}</p>
+                {props.printingIngredients(props.data.tuesdayMealIngredients)}
+                <p>Original Meal Source: {props.data.tuesdayMealSource}</p>
                 <button onClick={() => props.changingMeal('tuesday')}>
                 Change Meal
                 </button>
             </div> :
-            <AddMeal meal={props.data.tuesdayMeal}
-            changingMeal={() => props.changingMeal('tuesday')}/>}
+            <div>
+                <h3>New Meal</h3>
+                <form>
+                    <input type='textbox' placeholder="Name of Meal"
+                    value={props.tuesdayMealLabel} name="tuesdayMealLabel"
+                    onChange={props.handleChange}/>
+                    <br/>
+                    <br/>
+
+                    <label>
+                        Ingredients (items separated by a comma and a single space)
+                    </label>
+                    <br/>
+                    <input type="text" value={props.tuesdayMealIngredients}
+                    name="tuesdayMealIngredients" onChange={props.handleChange}/>
+                    <br/>
+                    <br/>
+                    <button onClick={() => props.changingMeal('tuesday')}>
+                    Submit
+                    </button>
+                </form>
+            </div>}
 
             <h2>Wednesday</h2>
             {!props.data.wednesdayMealEditing ?
             <div>
-                <h3>{props.data.wednesdayMeal.label}</h3>
+                <h3>{props.data.wednesdayMealLabel}</h3>
                 <p>Ingredients</p>
-                {props.printingIngredients(props.data.wednesdayMeal.ingredientLines)}
-                <p>Original Meal Source: {props.data.wednesdayMeal.source}</p>
+                {props.printingIngredients(props.data.wednesdayMealIngredients)}
+                <p>Original Meal Source: {props.data.wednesdayMealSource}</p>
                 <button onClick={() => props.changingMeal('wednesday')}>
                 Change Meal
                 </button>
             </div>:
-            <AddMeal meal={props.data.wednesdayMeal}
-            changingMeal={() => props.changingMeal('wednesday')}/>}
+            <div>
+                <h3>New Meal</h3>
+                <form>
+                    <input type='textbox' placeholder="Name of Meal"
+                    value={props.wednesdayMealLabel} name="wednesdayMealLabel"
+                    onChange={props.handleChange}/>
+                    <br/>
+                    <br/>
+
+                    <label>
+                        Ingredients (items separated by a comma and a single space)
+                    </label>
+                    <br/>
+                    <input type="text" value={props.wednesdayMealIngredients}
+                    name="wednesdayMealIngredients" onChange={props.handleChange}/>
+                    <br/>
+                    <br/>
+                    <button onClick={() => props.changingMeal('wednesday')}>
+                    Submit
+                    </button>
+                </form>
+            </div>}
 
             <h2>Thursday</h2>
             {!props.data.thursdayMealEditing ?
             <div>
-                <h3>{props.data.thursdayMeal.label}</h3>
+                <h3>{props.data.thursdayMealLabel}</h3>
                 <p>Ingredients</p>
-                {props.printingIngredients(props.data.thursdayMeal.ingredientLines)}
-                <p>Original Meal Source: {props.data.thursdayMeal.source}</p>
+                {props.printingIngredients(props.data.thursdayMealIngredients)}
+                <p>Original Meal Source: {props.data.thursdayMealSource}</p>
                 <button onClick={() => props.changingMeal('thursday')}>
                 Change Meal
                 </button>
             </div>:
-            <AddMeal meal={props.data.thursdayMeal}
-            changingMeal={() => props.changingMeal('thursday')}/>}
+            <div>
+                <h3>New Meal</h3>
+                <form>
+                    <input type='textbox' placeholder="Name of Meal"
+                    value={props.thursdayMealLabel} name="thursdayMealLabel"
+                    onChange={props.handleChange}/>
+                    <br/>
+                    <br/>
+
+                    <label>
+                        Ingredients (items separated by a comma and a single space)
+                    </label>
+                    <br/>
+                    <input type="text" value={props.thursdayMealIngredients}
+                    name="thursdayMealIngredients" onChange={props.handleChange}/>
+                    <br/>
+                    <br/>
+                    <button onClick={() => props.changingMeal('thursday')}>
+                    Submit
+                    </button>
+                </form>
+            </div>}
 
             <h2>Friday</h2>
             {!props.data.fridayMealEditing ?
             <div>
-                <h3>{props.data.fridayMeal.label}</h3>
+                <h3>{props.data.fridayMealLabel}</h3>
                 <p>Instructions</p>
-                {props.printingIngredients(props.data.fridayMeal.ingredientLines)}
-                <p>Original Meal Source: {props.data.fridayMeal.source}</p>
+                {props.printingIngredients(props.data.fridayMealIngredients)}
+                <p>Original Meal Source: {props.data.fridayMealSource}</p>
                 <button onClick={() => props.changingMeal('friday')}>
                 Change Meal
                 </button>
             </div> :
-            <AddMeal meal={props.data.fridayMeal}
-            changingMeal={() => props.changingMeal('friday')}/>}
+            <div>
+                <h3>New Meal</h3>
+                <form>
+                    <input type='textbox' placeholder="Name of Meal"
+                    value={props.fridayMealLabel} name="fridayMealLabel"
+                    onChange={props.handleChange}/>
+                    <br/>
+                    <br/>
+
+                    <label>
+                        Ingredients (items separated by a comma and a single space)
+                    </label>
+                    <br/>
+                    <input type="text" value={props.fridayMealIngredients}
+                    name="fridayMealIngredients" onChange={props.handleChange}/>
+                    <br/>
+                    <br/>
+                    <button onClick={() => props.changingMeal('friday')}>
+                    Submit
+                    </button>
+                </form>
+            </div>}
 
             <h2>Saturday</h2>
             {!props.data.saturdayMealEditing ?
             <div>
-                <h3>{props.data.saturdayMeal.label}</h3>
+                <h3>{props.data.saturdayMealLabel}</h3>
                 <p>Instructions</p>
-                {props.printingIngredients(props.data.saturdayMeal.ingredientLines)}
-                <p>Original Meal Source: {props.data.saturdayMeal.source}</p>
+                {props.printingIngredients(props.data.saturdayMealIngredients)}
+                <p>Original Meal Source: {props.data.saturdayMealSource}</p>
                 <button onClick={() => props.changingMeal('saturday')}>
                 Change Meal
                 </button>
             </div>:
-            <AddMeal meal={props.data.saturdayMeal}
-            changingMeal={() => props.changingMeal('saturday')}/>}
+            <div>
+                <h3>New Meal</h3>
+                <form>
+                    <input type='textbox' placeholder="Name of Meal"
+                    value={props.saturdayMealLabel} name="saturdayMealLabel"
+                    onChange={props.handleChange}/>
+                    <br/>
+                    <br/>
+
+                    <label>
+                        Ingredients (items separated by a comma and a single space)
+                    </label>
+                    <br/>
+                    <input type="text" value={props.saturdayMealIngredients}
+                    name="saturdayMealIngredients" onChange={props.handleChange}/>
+                    <br/>
+                    <br/>
+                    <button onClick={() => props.changingMeal('saturday')}>
+                    Submit
+                    </button>
+                </form>
+            </div>}
 
             <h2>Sunday</h2>
             {!props.data.sundayMealEditing ?
             <div>
-                <h3>{props.data.sundayMeal.label}</h3>
+                <h3>{props.data.sundayMealLabel}</h3>
                 <p>Instructions</p>
-                {props.printingIngredients(props.data.sundayMeal.ingredientLines)}
-                <p>Original Meal Source: {props.data.sundayMeal.source}</p>
-                <button onClick={() => props.changinMeal('sunday')}>
+                {props.printingIngredients(props.data.sundayMealIngredients)}
+                <p>Original Meal Source: {props.data.sundayMealSource}</p>
+                <button onClick={() => props.changingMeal('sunday')}>
                 Change Meal
                 </button>
             </div> :
-            <AddMeal meal={props.data.sundayMeal}
-            changingMeal={() => props.changinMeal('sunday')}/>}
+            <div>
+                <h3>New Meal</h3>
+                <form>
+                    <input type='textbox' placeholder="Name of Meal"
+                    value={props.sundayMealLabel} name="sundayMealLabel"
+                    onChange={props.handleChange}/>
+                    <br/>
+                    <br/>
+
+                    <label>
+                        Ingredients (items separated by a comma and a single space)
+                    </label>
+                    <br/>
+                    <input type="text" value={props.sundayMealIngredients}
+                    name="sundayMealIngredients" onChange={props.handleChange}/>
+                    <br/>
+                    <br/>
+                    <button onClick={() => props.changingMeal('sunday')}>
+                    Submit
+                    </button>
+                </form>
+            </div>}
 
         </div>
     )
--- src/Components/CurrentMealsContainer.js
+++ src/Components/CurrentMealsContainer.js
@@ -9,23 +9,50 @@ class CurrentMealsContainer extends React.Component {
             mondayMeal: this.props.data.mondayMeal,
             mondayMealLabel: this.props.data.mondayMeal.label,
             mondayMealIngredients: this.props.data.mondayMeal.ingredientLines,
+            mondayMealSource: this.props.data.mondayMeal.source,
             mondayMealEditing: false,
+
             tuesdayMeal: this.props.data.tuesdayMeal,
+            tuesdayMealLabel: this.props.data.tuesdayMeal.label,
+            tuesdayMealIngredients: this.props.data.tuesdayMeal.ingredientLines,
+            tuesdayMealSource: this.props.data.tuesdayMeal.source,
             tuesdayMealEditing: false,
+
             wednesdayMeal: this.props.data.wednesdayMeal,
+            wednesdayMealLabel: this.props.data.wednesdayMeal.label,
+            wednesdayMealIngredients: this.props.data.wednesdayMeal.ingredientLines,
+            wednesdayMealSource: this.props.data.wednesdayMeal.source,
             wednesdayMealEditing: false,
+
             thursdayMeal: this.props.data.thursdayMeal,
+            thursdayMealLabel: this.props.data.thursdayMeal.label,
+            thursdayMealIngredients: this.props.data.thursdayMeal.ingredientLines,
+            thursdayMealSource: this.props.data.thursdayMeal.source,
             thursdayMealEditing: false,
+
             fridayMeal: this.props.data.fridayMeal,
+            fridayMealLabel: this.props.data.fridayMeal.label,
+            fridayMealIngredients: this.props.data.fridayMeal.ingredientLines,
+            fridayMealSource: this.props.data.fridayMeal.source,
             fridayMealEditing: false,
+
             saturdayMeal: this.props.data.saturdayMeal,
+            saturdayMealLabel: this.props.data.saturdayMeal.label,
+            saturdayMealIngredients: this.props.data.saturdayMeal.ingredientLines,
+            saturdayMealSource: this.props.data.saturdayMeal.source,
             saturdayMealEditing: false,
+
             sundayMeal: this.props.data.sundayMeal,
+            sundayMealLabel: this.props.data.sundayMeal.label,
+            sundayMealIngredients: this.props.data.sundayMeal.ingredientLines,
+            sundayMealSource: this.props.data.sundayMeal.source,
             sundayMealEditing: false,
+
             mealBeingChanged: ""
         }
         this.printingIngredients = this.printingIngredients.bind(this)
         this.changingMeal = this.changingMeal.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
 
     printingIngredients(ingredientList) {
@@ -36,26 +63,58 @@ class CurrentMealsContainer extends React.Component {
 
     changingMeal(meal) {
         if (meal === 'monday') {
-            this.setState({mondayMealEditing: !this.state.mondayMealEditing})
+            this.setState({
+                mondayMealEditing: !this.state.mondayMealEditing,
+                mondayMealSource: "User"
+            })
         }
         else if (meal === 'tuesday') {
-            this.setState({tuesdayMealEditing: !this.state.tuesdayMealEditing})
+            this.setState({
+                tuesdayMealEditing: !this.state.tuesdayMealEditing,
+                tuesdayMealSource: "User"
+            })
         }
         else if (meal === 'wednesday') {
-            this.setState({wednesdayMealEditing: !this.state.wednesdayMealEditing})
+            this.setState({
+                wednesdayMealEditing: !this.state.wednesdayMealEditing,
+                wednesdayMealSource: "User"
+            })
         }
         else if (meal === 'thursday') {
-            this.setState({thursdayMealEditing: ! this.state.thursdayMealEditing})
+            this.setState({
+                thursdayMealEditing: ! this.state.thursdayMealEditing,
+                thursdayMealSource: "User"
+            })
         }
         else if (meal === 'friday') {
-            this.setState({fridayMealEditing: !this.state.fridayMealEditing})
+            this.setState({
+                fridayMealEditing: !this.state.fridayMealEditing,
+                fridayMealSource: "User"
+            })
         }
         else if (meal === 'saturday') {
-            this.setState({saturdayMealEditing: !this.state.saturdayMealEditing})
+            this.setState({
+                saturdayMealEditing: !this.state.saturdayMealEditing,
+                saturdayMealSource: "User"
+            })
         }
         else if (meal === 'sunday') {
-            this.setState({sundayMealEditing: !this.state.sundayMealEditing})
+            this.setState({
+                sundayMealEditing: !this.state.sundayMealEditing,
+                sundayMealSource: "User"
+            })
         }
+    }
+
+    handleChange(event){
+        let {name, value} = event.target
+        if (name.includes('Ingredients')) {
+            let arrayIngredients = value.split(',')
+            value = arrayIngredients
+        }
+        this.setState({
+            [name]: value,
+        })
     }
 
     render() {
@@ -63,6 +122,7 @@ class CurrentMealsContainer extends React.Component {
             <CurrentMealsComponent
             printingIngredients={this.printingIngredients}
             changingMeal={this.changingMeal}
+            handleChange={this.handleChange}
             data={this.state}/>
         )
     }
