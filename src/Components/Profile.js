7217fcfcbfbd889e259a24c0cddcc6a587a36c31
Matthew Meyerink <meyerink@umich.edu>
2020-08-18 13:25:40 -0500

    Added placeholder to profile page

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import './Components.css'
+
 class Profile extends React.Component {
 
     render(){
         return(
-            <div>
-                This is going to be the profile page
+            <div className="profile_page">
+                <h1 className="profile_placeholder">Profile feature coming soon!</h1>
             </div>
         )
     }
